768f6349a0eb58961f82a70020182755c8e90bf9
mdm177 <quyenmoscoso@gmail.com>
2022-10-21 21:06:01 -0500

    added endpoint and function to show all users. working on frontend

--- src/main/java/com/mandy/capstone/controllers/AdminController.java
+++ src/main/java/com/mandy/capstone/controllers/AdminController.java
@@ -1,26 +1,31 @@
 package com.mandy.capstone.controllers;
 
 import com.mandy.capstone.dtos.UserDto;
+import com.mandy.capstone.entities.User;
+import com.mandy.capstone.repositories.UserRepository;
 import com.mandy.capstone.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@Controller
+@RestController
 @RequestMapping
 public class AdminController {
     @Autowired
     private UserService userService;
     @Autowired
     private PasswordEncoder passwordEncoder;
+    @Autowired
+    private UserRepository userRepository;
 
     @PostMapping("/admin/createuser/{role}")
     //@Responsebody:  annotation can be put on a method and indicates that the return type should be written straight to the HTTP response body (and not placed in a Model, or interpreted as a view name).
     //need it here because I used @Controller vs @RestController
-    @ResponseBody
+//    @ResponseBody
     public List<String> addAcount(@RequestBody UserDto newUser, @PathVariable String role){
         String passHash = passwordEncoder.encode(newUser.getPassword());
         newUser.setPassword(passHash);
@@ -28,15 +33,18 @@ public class AdminController {
         return userService.addNewAccount(newUser, role);
     }
 
-//I am using MvcConfig for view, so no need for this get request. This is just an alternative way to get the job done. MvcConfig has all view in 1 place so easier to keep track.
+      @GetMapping("admin/getborrower")
+    public List <User> showAllUser(){
+        return  userRepository.findAll();
+    }
+
+//I am using MvcConfig for view, so no need for this get request. This is just an alternative way to get the job done. MvcConfig has all view in 1 place so easier to keep track. To use this, need to change to @Controller
 //    @GetMapping("/adminnewuser")
 //    public String getCreateUserPage(){
 //        return "admin-create-user";
 //    }
-//
-//    @GetMapping("/adminhome")
-//    public String getAdminHomePage(){
-//        return "admindashboard";
-//    }
+
+
+
 
 }
